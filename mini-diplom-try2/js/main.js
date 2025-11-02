6083cd806e754d6620e792278cd28b58081e24bc
Shasotari <132165426+Shasotari@users.noreply.github.com>
2025-11-03 00:23:15 +0300

    Add files via upload

--- mini-diplom-try2/js/main.js
+++ mini-diplom-try2/js/main.js
@@ -5,9 +5,6 @@
     function burgerMenu(m) {
         
         const burgerIcon = m.target.closest('.burger-icon')
-        if (burgerIcon) {
-            console.log('WWWWWWWWW');
-        }
         const burgerNavLink = m.target.closest('.nav__link')
 
         if (!burgerIcon && !burgerNavLink) return
@@ -19,6 +16,36 @@
             document.body.classList.remove('body--opened-menu')
         }
 
+    }
+
+})();
+(function() {
+    
+    document.addEventListener('click', modalW)
+
+    function modalW(w) {
+        
+        const giftButton = w.target.closest('.button-gift')
+        const closeModal = w.target.closest('.modal__cancel')
+        const modalButton = w.target.closest('.modal__button')
+        const modal = w.target.closest('.modal')
+        const modalWindow = w.target.closest('.modal__window')
+        
+        if (giftButton) {
+            document.body.classList.add('body--modal-opened')
+            return
+        }
+        
+        
+        if (closeModal || modalButton) {
+            document.body.classList.remove('body--modal-opened')
+            return
+        }
+        
+        
+        if (modal && !modalWindow) {
+            document.body.classList.remove('body--modal-opened')
+        }
     }
 
 })()
