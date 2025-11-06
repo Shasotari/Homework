ca8cb2c558a95d5a75fccbdcdf6aab959a932a45
Shasotari <132165426+Shasotari@users.noreply.github.com>
2025-11-06 06:06:49 +0300

    Add files via upload

--- mini-diplom-try2/js/main.js
+++ mini-diplom-try2/js/main.js
@@ -1,3 +1,4 @@
+// ============================= бургер =================================== //
 (function() {
     
     document.addEventListener('click', burgerMenu)
@@ -19,8 +20,9 @@
     }
 
 })();
+// ============================================================================
 (function() {
-    
+    // модалка =============================================================
     document.addEventListener('click', modalW)
 
     function modalW(w) {
@@ -47,5 +49,63 @@
             document.body.classList.remove('body--modal-opened')
         }
     }
+// табы ===============================================================================================
+
+    const tabControls = document.querySelector('.tab-controls')
+
+    tabControls.addEventListener('click', toggleTab)
+
+    function toggleTab(e) {
+
+        const tabControl = e.target.closest('.tab-controls__link')
+
+        if (!tabControl) return
+        e.preventDefault()
+        if (tabControl.classList.contains('tab-controls__link--active')) return
+
+
+        const tabContentID = tabControl.getAttribute('href')
+        const tabContent = document.querySelector(tabContentID)
+        const activeControl = document.querySelector('.tab-controls__link--active')
+        const activeContent = document.querySelector('.tab-content--show')
+
+        activeContent.classList.remove('tab-content--show')
+        tabContent.classList.add('tab-content--show')
+        
+        activeControl.classList.remove('tab-controls__link--active')
+        tabControl.classList.add('tab-controls__link--active')
+    }
+//  Аккордеон ===============================================================================
+        const accordionLists = document.querySelectorAll('.accordion-list');
+
+        accordionLists.forEach(el => {
+
+            el.addEventListener('click', (e) => {
+
+            const accordionList = e.currentTarget
+            const accordionOpenedItem = accordionList.querySelector('.accordion-list__item--opened')
+            const accordionOpenedContent = accordionList.querySelector('.accordion-list__item--opened .accordion-list__content')
+
+            const accordionControl = e.target.closest('.accordion-list__control');
+            if (!accordionControl) return
+            const accordionItem = accordionControl.parentElement;
+            const accordionContent = accordionControl.nextElementSibling;
+
+            if (accordionOpenedItem && accordionItem != accordionOpenedItem) {
+                accordionOpenedItem.classList.remove('accordion-list__item--opened');
+                accordionOpenedContent.style.maxHeight = null;
+            }
+            accordionItem.classList.toggle('accordion-list__item--opened');
+
+            if (accordionItem.classList.contains('accordion-list__item--opened')) {
+                accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+            } else {
+                accordionContent.style.maxHeight = null;
+            }
+
+            }); 
+
+        });
+
 
 })()
